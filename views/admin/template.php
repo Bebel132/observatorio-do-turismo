472696a06d2762ad116a78d5c122fd0af1ddd17e
Prodacy QS <prodacy.soares@gmail.com>
2020-11-05 17:18:42 -0300

    remove title

--- views/admin/template.php
+++ views/admin/template.php
@@ -23,8 +23,8 @@
 
 	<script> jq = jQuery.noConflict( true ); </script>
 	<script src='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/js/bootstrap-datepicker.min.js'></script>
-	<script> $(document).ready(function(){jq('input[type=datetime-local]').datepicker({format: "yy-mm-dd"}); }) </script>
-	<script> $(document).ready(function(){jq('input[type=datetime]').datepicker({format: "yyyy-mm-dd"}); }) </script>
+	<script> jq(document).ready(function(){jq('input[type=datetime-local]').datepicker({format: "yy-mm-dd"}); }) </script>
+	<script> jq(document).ready(function(){jq('input[type=datetime]').datepicker({format: "yyyy-mm-dd"}); }) </script>
 	<?= FrontEnd::resource('admin.js'); ?>
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
