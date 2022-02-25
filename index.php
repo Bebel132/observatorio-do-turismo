ad47ca02b3f42fce9932b05e54514df6e25f096f
Prodacy QS <prodacy.soares@gmail.com>
2022-02-25 15:32:12 -0300

    changes

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 session_start();
 
+ini_set('display_errors', TRUE );
+
 // helper function
 function require_path($diretorio){$dir = dir($diretorio);while($arquivo = $dir->read()){if(substr($arquivo, -4,4) == '.php'){include_once($diretorio.$arquivo);}}$dir -> close();}
 
